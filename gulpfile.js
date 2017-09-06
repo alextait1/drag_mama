93c41418a413bfeb90782e1b48031c2ba2f9561d
Alex Tait <alexandratait@gmail.com>
2017-09-06 12:50:22 -0400

    updated with router packages

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ const notify = require('gulp-notify');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
+const historyApiFallback = require('connect-history-api-fallback');
 
 // sass setup
 gulp.task('styles', () => {
@@ -39,11 +40,24 @@ gulp.task('js', () => {
 });
 
 // browsersync
+
 gulp.task('bs', () => {
     browserSync.init({
         server: {
             baseDir: './'
-        }
+        },
+        middleware: [historyApiFallback()] // <-- add this line
+    });
+});
+
+const historyApiFallback = require('connect-history-api-fallback');
+
+gulp.task('bs', () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        middleware: [historyApiFallback()] // <-- add this line
     });
 });
 
