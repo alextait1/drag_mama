7738f12db2e73dad73c8a22f1b51f0e89641bba4
Alexandra  Tait <alexandratait@Alexandras-MacBook-Air.local>
2017-09-08 15:07:43 -0400

    initial commit

---
+++ dev/scripts/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Form extends React.Component {
+	render (){
+		return (
+			<header>
+				<img src="assets/rupaul1.jpg" alt="image of RuPaul" className="rupaul__image"/>
+				<h1>Who's Your Fairy Drag Mother?</h1>
+				<h2>Spill the T to find your drag spirit guide.</h2>
+			<header>
+			)
+	}
+}
+
+export default Form;
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Form from './Form'
 import { 
     BrowserRouter as Router, 
     Route, Link } from 'react-router-dom';
@@ -7,19 +8,23 @@ import {
 class App extends React.Component {
 	render (){
 		return (
-			<div>
-				<img src="assets/rupaul1.jpg" alt=""/>
+			<header>
+				<img src="assets/rupaul1.jpg" alt="image of RuPaul" className="rupaul__image"/>
 				<h1>Who's Your Fairy Drag Mother?</h1>
 				<h2>Spill the T to find your drag spirit guide.</h2>
-			</div>
+			<header>
 			)
 	}
 }
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // Queens:
 
 // Create the App component to hold everything
 // set up components and and initial state
+// components: top level: header, results
+// header components: image div (simple), animated speech bubble with questions in it (form)
 // RuPaul image component maybe with an animated mouth?
 // Speech bubble complex component with dynamic questions being animated, 
 // include a button to store that data and call the next question
@@ -51,5 +56,5 @@ class App extends React.Component {
 // alaska https://www.youtube.com/watch?v=k2A2EQYccOY
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
 
